7ad19695cd9a8848653fb57b714cdcef6fc4e718
memsom <memsom.spam@gmail.com>
2016-03-22 14:09:52 +0000

    Fixed a bug in the assembler SM_B's register allocation that meant the jump table for the sub routines was corrupted.

---
+++ src/GUITestbed/bin/Debug/Examples/prog1.asm
@@ -0,0 +1,28 @@
+﻿        ORG START 
+        NOP                              		                             
+START:  ST_B 0          ; store byte value 0 on stack
+        LD_B V1 ;  x    ; put value on stack in to the V1 register
+        ST_B 0          ; store byte value 0 on stack
+        LD_B V2 ;  y    ; put value on stack in to the V2 register
+        NOP
+LOOP:   SM_B V1         ; store V1 on stack
+        SM_B V2         ; store V1 on stack
+        ST_B 0          ; store 0 on stack
+        TRP 25          ; call trap 25 (TRP_GUIDRAW) (this takes c, y, x from stack and calls CallGraphicsOutputEvent(...))
+        NOP
+        SM_B V1         ; store V1 on stack
+        ST_B 1          ; store 1 on stack
+        ADD_B           ; ADD last two values on stack, leaving the result on the stack
+        LD_B V1         ; put stack value in V1
+        NOP
+        SM_B V2         ; store V2 on stack
+        ST_B 1			; store 1 on stack
+        ADD_B			; ADD last two values on stack, leaving the result on the stack
+        LD_B V2			; put stack value in V2
+        NOP
+        SM_B V1
+        CMP_B 255
+        JNE LOOP        ; repeat
+        NOP
+STOP:   NOP
+        END
--- src/GUITestbed/bin/Debug/prog.asm
+++ src/GUITestbed/bin/Debug/prog.asm
@@ -1,28 +1,15 @@
-﻿        ORG START 
-        NOP                              		                             
-START:  ST_B 0          ; store byte value 0 on stack
+﻿        ORG START                               		                             
+START:  ST_B 10         ; store byte value 0 on stack
         LD_B V1 ;  x    ; put value on stack in to the V1 register
-        ST_B 0          ; store byte value 0 on stack
-        LD_B V2 ;  y    ; put value on stack in to the V2 register
-        NOP
-LOOP:   SM_B V1         ; store V1 on stack
+        ST_B 10         ; store byte value 0 on stack
+        LD_B V2 ;  y    ; put value on stack in to the V2 register        
+        JSR PROG        
+        JMP STOP
+PROG:   JSR PLOT      
+        RSR
+PLOT:   SM_B V1         ; store V1 on stack
         SM_B V2         ; store V1 on stack
         ST_B 0          ; store 0 on stack
         TRP 25          ; call trap 25 (TRP_GUIDRAW) (this takes c, y, x from stack and calls CallGraphicsOutputEvent(...))
-        NOP
-        SM_B V1         ; store V1 on stack
-        ST_B 1          ; store 1 on stack
-        ADD_B           ; ADD last two values on stack, leaving the result on the stack
-        LD_B V1         ; put stack value in V1
-        NOP
-        SM_B V2         ; store V2 on stack
-        ST_B 1			; store 1 on stack
-        ADD_B			; ADD last two values on stack, leaving the result on the stack
-        LD_B V2			; put stack value in V2
-        NOP
-        SM_B V1
-        CMP_B 255
-        JNE LOOP        ; repeat
-        NOP
-STOP:   NOP
-        END
+        RSR       
+STOP:   END
